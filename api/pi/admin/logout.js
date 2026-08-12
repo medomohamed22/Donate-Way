54d5ef85e926d4a79d52a0bde795b273df2d1004
medomohamed22 <123499261+medomohamed22@users.noreply.github.com>
2026-08-12 10:14:44 +0300

    Delete api/pi directory

--- api/pi/admin/logout.js
+++
@@ -1,2 +0,0 @@
-const {cookie}=require('../../lib/backend.cjs');
-module.exports=async function(req,res){if(req.method!=='POST')return res.status(405).json({error:'Method Not Allowed'});res.setHeader('Set-Cookie',cookie('',0));return res.status(200).json({ok:true})};
